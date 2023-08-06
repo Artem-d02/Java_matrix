d0e182e9160bb7dc8cdd787bf4cae65486668867
Artem-d02 <artemd2002@yandex.ru>
2023-08-06 22:25:39 +0300

    Small refactoring: matrix was shifted in package Mtrx

--- src/Mtrx/Matrix.java
+++ src/Mtrx/Matrix.java
@@ -1,16 +1,29 @@
+package Mtrx;
+
 import java.util.ArrayList;
 
 public class Matrix<T extends Number> {
     private ArrayList<ArrayList<T>> matrix;
     private int sizeX;
     private int sizeY;
-    Matrix(final int sizeX, final int sizeY) {
+    public Matrix(final int sizeX, final int sizeY) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         matrix = new ArrayList<>(sizeY);
         for (int rawIndex = 0; rawIndex < sizeY; rawIndex++) {
             matrix.add(rawIndex, new ArrayList<>(sizeX));
         }
+    }
+    public Matrix(Matrix<T> anotherMat) {
+        this(anotherMat.sizeX, anotherMat.sizeY);
+        for (int yIndex = 0; yIndex < sizeY; yIndex++) {
+            matrix.set(yIndex, (ArrayList<T>) anotherMat.getRaw(yIndex).clone());
+        }
+    }
+    @Override
+    public Matrix<T> clone() {
+        Matrix<T> result = new Matrix<>(this);
+        return result;
     }
     public int getSizeX() {
         return sizeX;
@@ -28,6 +41,11 @@ public class Matrix<T extends Number> {
         if (newRaw.size() != sizeX)
             throw new IllegalArgumentException("Fatal error: illegal size of raw");
         matrix.set(index, newRaw);
+    }
+    public ArrayList<T> getRaw(final int index) throws IllegalStateException {
+        if (index >= sizeY)
+            throw new IllegalArgumentException("Fatal error: illegal size of raw");
+        return matrix.get(index);
     }
     public void print(java.io.PrintStream stream) {
         for (ArrayList<T> raw : matrix) {
